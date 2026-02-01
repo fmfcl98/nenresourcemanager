499d5c298cc05035b638da805dfa7cec5a7392b1
mrlusitano <filipemfcl@outlook.com>
2026-02-01 23:33:53 +0100

    Util functions and more commnets

--- include/ResourceManager/ResourceManager.h
+++ include/ResourceManager/ResourceManager.h
@@ -6,14 +6,35 @@
 #include "IResourceFactory.h"
 #include "ILoader.h"
 
-namespace nenresourcemanager {
-    class ResourceManager {
+namespace nenresourcemanager
+{
+    /// @brief Manages resource loading, caching, and factory registration.
+    class ResourceManager
+    {
     public:
-        explicit ResourceManager(ILoader& loader) : m_loader(loader) {}
+        /// @brief Constructs a ResourceManager with a given loader.
+        /// @param loader The loader used to load raw data.
+        explicit ResourceManager(ILoader &loader) : m_loader(loader) {}
 
+        /// @brief Registers a resource factory for a specific resource type.
+        /// @tparam T The type of the resource.
+        /// @param factory A unique pointer to the resource factory.
         template <typename T>
-        void RegisterFactory(std::unique_ptr<IResourceFactory> factory) {
+        void RegisterFactory(std::unique_ptr<IResourceFactory> factory)
+        {
             m_factories[typeid(T)] = std::move(factory);
+        }
+
+        /// @brief Clears the resource cache, removing all loaded resources from RAM.
+        void clearCache()
+        {
+            m_resourceCache.clear();
+        }
+
+        /// @brief Clears all registered resource factories.
+        void clearFactories()
+        {
+            m_factories.clear();
         }
 
         /// @brief Loads a resource of a given type from a specified path.
@@ -21,7 +42,8 @@ namespace nenresourcemanager {
         /// @param path The file path to load the resource from.
         /// @return A shared pointer to the loaded resource.
         template <typename T>
-        std::shared_ptr<T> Load(const std::string &path) {
+        std::shared_ptr<T> Load(const std::string &path)
+        {
             return std::static_pointer_cast<T>(LoadInternal(path, typeid(T)));
         }
 
@@ -30,13 +52,13 @@ namespace nenresourcemanager {
         /// @param path The file path to load the resource from.
         /// @param type The type of the resource to load.
         /// @return A shared pointer to the loaded resource.
-        std::shared_ptr<void> LoadInternal(const std::string& path, std::type_index type);
+        std::shared_ptr<void> LoadInternal(const std::string &path, std::type_index type);
 
         /// @brief Map of resource factories indexed by resource type.
         std::unordered_map<std::type_index, std::unique_ptr<IResourceFactory>> m_factories;
         /// @brief Cache of loaded resources indexed by a combination of path and type.
         std::unordered_map<std::string, std::shared_ptr<void>> m_resourceCache;
         /// @brief Reference to the loader used to load raw data.
-        ILoader& m_loader;
+        ILoader &m_loader;
     };
 }
