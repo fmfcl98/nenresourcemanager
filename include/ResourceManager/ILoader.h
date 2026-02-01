586253ff33c546238ee044efa697a27c36f4320b
mrlusitano <filipemfcl@outlook.com>
2026-02-01 22:15:24 +0100

    comments

--- include/ResourceManager/ILoader.h
+++ include/ResourceManager/ILoader.h
@@ -4,11 +4,15 @@
 
 namespace nenresourcemanager
 {
+    /// @brief Interface for loaders that load raw data from files.
     class ILoader
     {
     public:
         virtual ~ILoader() = default;
 
+        /// @brief Loads raw data from a specified file path.   
+        /// @param path The file path to load the raw data from.
+        /// @return A vector containing the loaded raw data.
         virtual std::vector<char> LoadRawData(const std::string &path) = 0;
     };
 }
